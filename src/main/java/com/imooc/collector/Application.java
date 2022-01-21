89b6d9ef17db2af6103efee7a77eb92965701138
SIYUAN XU <xsiyuan@gmail.com>
2022-01-21 09:54:51 +0800

    日志收集项目准备

---
+++ src/main/java/com/imooc/collector/Application.java
@@ -0,0 +1,13 @@
+package com.imooc.collector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/** @author afu */
+@SpringBootApplication
+public class Application {
+  public static void main(String[] args) {
+    SpringApplication app = new SpringApplication(Application.class);
+    app.run(args);
+  }
+}
